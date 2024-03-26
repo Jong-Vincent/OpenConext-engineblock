2338448d2df4d2417008c8505546fd92198a8672
Vincent Jong <jong.vincent@kpmg.nl>
2024-03-26 14:19:07 +0100

    Initial upgrade to Symfony 5.4

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -13,11 +13,11 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+//            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
 
             // Sensio helpers
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle(),
+//            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+//            new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle(),
 
             // Doctrine integration
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
@@ -43,7 +43,7 @@ class AppKernel extends Kernel
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $configurationDirectory = $this->getRootDir() . '/config/';
+        $configurationDirectory = $this->getProjectDir() . '/app/config/';
         $loader->load($configurationDirectory . 'config_' . $this->getEnvironment() . '.yml');
 
         $localConfiguration = $configurationDirectory . 'config_local.yml';
@@ -60,12 +60,12 @@ class AppKernel extends Kernel
 
     public function getCacheDir()
     {
-        return $this->rootDir . '/cache/' . $this->environment;
+        return $this->getProjectDir() . '/app/cache/' . $this->environment;
     }
 
     public function getLogDir()
     {
-        return $this->rootDir . '/logs/' . $this->environment;
+        return $this->getProjectDir() . '/app/logs/' . $this->environment;
     }
 
     public function getProjectDir()
