7cdfd3e55a8094b80e3e98bf3ab18c76827ff3e5
Vincent Jong <jong.vincent@kpmg.nl>
2024-04-30 14:19:59 +0200

    Remove obsolete bundles and switch to Symfony Mailer

--- library/EngineBlock/Application/DiContainer.php
+++ library/EngineBlock/Application/DiContainer.php
@@ -283,7 +283,7 @@ class EngineBlock_Application_DiContainer extends Pimple
     }
 
     /**
-     * @return Swift_Mailer
+     * @return \Symfony\Component\Mailer\MailerInterface
      */
     public function getMailer()
     {
